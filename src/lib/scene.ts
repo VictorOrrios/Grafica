c7e75948ed76dbc697714c98ceba7e6b65f2a8d9
jmquilez <finalizer2869@gmail.com>
2025-11-23 15:45:09 +0100

    checkpoint (IDE change)

--- src/lib/scene.ts
+++ src/lib/scene.ts
@@ -807,10 +807,10 @@ export class Scene {
             this.addMesh(tralaleroMesh, yellow);*/
             // const tralaleroMesh = await ThreeJSOBJLoader.load("models/obj/tralalero/original/model.obj");
             // const tralaleroMesh = await ThreeJSOBJLoader.load("models/obj/skull/skull.obj");
-            // const tralaleroMesh = await ThreeJSOBJLoader.load("tetrahedron/pyramid.obj");
+            // const tralaleroMesh = await ThreeJSOBJLoader.load("models/obj/tetrahedron/pyramid.obj");
             // const tralaleroMesh = await ThreeJSOBJLoader.load("models/obj/hexahedron/scene.obj");
-            const tralaleroMesh = await ThreeJSOBJLoader.load("models/obj/dodecahedron/dodecahedron.obj");
-            // const tralaleroMesh = await ThreeJSOBJLoader.load("models/obj/skull-detailed/craneo.obj");
+            // const tralaleroMesh = await ThreeJSOBJLoader.load("models/obj/dodecahedron/dodecahedron.obj");
+            const tralaleroMesh = await ThreeJSOBJLoader.load("models/obj/skull-detailed/craneo.obj");
             this.addEfficientMeshData(tralaleroMesh);
             console.log("✓ Tralalero mesh loaded successfully");
         } catch (error) {
